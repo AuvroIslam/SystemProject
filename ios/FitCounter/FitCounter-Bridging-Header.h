e55207aaae1ec5e641a15eee977e9a2492a12d02
AuvroIslam <crossgame2002@gmail.com>
2026-04-21 20:25:38 +0600

    feat: add ios app shell and project files

---
+++ ios/FitCounter/FitCounter-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  FitCounter-Bridging-Header.h
+//
+//  Bridging header for Swift ↔ Objective-C interop.
+//  This enables the PoseDetectionPlugin (Swift) to be used from the
+//  registration macro (Objective-C).
+//
+
+#ifndef FitCounter_Bridging_Header_h
+#define FitCounter_Bridging_Header_h
+
+#import <VisionCamera/FrameProcessorPlugin.h>
+#import <VisionCamera/Frame.h>
+
+#endif /* FitCounter_Bridging_Header_h */
